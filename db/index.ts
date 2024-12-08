a951c5aa784ec53c5bd577008565216df3bc323b
Carlo D'Ugo <carlo.dugo@sitechhs.com>
2024-12-08 12:38:05 -0500

    updated index file

--- db/index.ts
+++ db/index.ts
@@ -3,22 +3,36 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
-	dialect: 'postgres',
-	logging: false, // Disable SQL query logging
-	dialectOptions: {
-		ssl:
-			process.env.NODE_ENV === 'production' ? { require: true, rejectUnauthorized: false } : false,
-	},
-});
+const isProduction = process.env.NODE_ENV === 'production';
 
-const connectToDatabase = async () => {
+const sequelize = isProduction
+	? new Sequelize(process.env.DATABASE_URL || '', {
+			dialect: 'postgres',
+			logging: false,
+			dialectOptions: {
+				ssl: {
+					require: true,
+					rejectUnauthorized: false,
+				},
+			},
+		})
+	: new Sequelize({
+			dialect: 'postgres',
+			host: process.env.POSTGRES_HOST,
+			port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+			logging: false,
+		});
+
+const connectToDatabase = async (): Promise<void> => {
 	try {
 		await sequelize.authenticate();
 		console.log('Connected to the database');
-	} catch (err: Error | unknown) {
-		console.error('Error connecting to the database', err);
-		process.exit(1); // Exit the application if the connection fails
+	} catch (err) {
+		console.error('Error connecting to the database:', err);
+		process.exit(1);
 	}
 };
 
